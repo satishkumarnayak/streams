3e70d8290bf33e2cc4dc9a402dbfbf391b8eca83
Satish Nayak <satish_nayak@epam.com>
2024-09-26 19:20:11 -0400

    stream numbers

--- src/com/instanceofcake/numbers/StreamsWithNumbers.java
+++ src/com/instanceofcake/numbers/StreamsWithNumbers.java
@@ -21,12 +21,36 @@ public class StreamsWithNumbers {
 
         // Using Streams
 
-       Arrays.stream(nums)
-                       .forEach(System.out::println);
+  //     Arrays.stream(nums)
+   //                    .forEach(System.out::println);
 
         sum = Arrays.stream(nums)
                 .sum();
-        System.out.println(sum);
+    //    System.out.println(sum);
+
+
+
+        // More
+
+     //   IntStream.range(0,5).forEach(x -> System.out.print(x+" "));
+
+        IntStream.range(0,10).filter(x -> x % 2 == 0).forEach(System.out::println);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
